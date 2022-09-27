0fc946e9d578fc78c5941a907afd0597cebb8405
kevin12509673 <kayiu99623@gmail.com>
2022-09-27 18:59:49 +0800

    Alter: hanlding animation by gsap

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="custom-btn">{{ label }}</div>
+  <div ref="custom-btn" class="custom-btn">{{ label }}</div>
 </template>
 
 <script lang="ts">
+import gsap from 'gsap';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -16,6 +17,22 @@ export default defineComponent({
   data() {
     return {
     };
+  },
+  mounted() {
+    (this.$refs['custom-btn'] as HTMLElement).addEventListener('mouseenter', () => {
+      gsap.to(this.$refs['custom-btn'] as gsap.TweenTarget, {
+        scale: 1.2,
+        boxShadow: '0px 0px 20px -3px map-get($map: $blue, $key: "200")',
+        duration: 0.1,
+      });
+    });
+    (this.$refs['custom-btn'] as HTMLElement).addEventListener('mouseleave', () => {
+      gsap.to(this.$refs['custom-btn'] as gsap.TweenTarget, {
+        scale: 1,
+        boxShadow: '0px 0px 10px -3px map-get($map: $blue, $key: "200")',
+        duration: 0.1,
+      });
+    });
   },
   methods: {
 
@@ -39,11 +56,12 @@ export default defineComponent({
   border-radius: $border-radius;
   cursor: pointer;
   font-size: 20px;
-  transition: transform 0.1s ease-in-out, box-shadow 0.15s ease-in-out;
+  // transition: transform 0.1s ease-in-out, box-shadow 0.15s ease-in-out;
+  // transition: transform 0.1s ease-in-out, box-shadow 0.15s ease-in-out;
   box-shadow: 0px 0px 20px -3px map-get($map: $blue, $key: "200");
-  &:hover {
-    transform: scale(1.2);
-    box-shadow: 0px 0px 30px -3px map-get($map: $blue, $key: "200");
-  }
+  // &:hover {
+  //   transform: scale(1.2);
+  //   box-shadow: 0px 0px 30px -3px map-get($map: $blue, $key: "200");
+  // }
 }
 </style>
