f7819994bfc5ca62cce412a9c4df3ef0d980ce04
kevin12509673 <kayiu99623@gmail.com>
2022-09-30 16:43:34 +0800

    Refactor: remove deprecated code

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,5 @@ import router from './router'
 import store from './store'
 import '@/style/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
-
-const a = true
-if (a) {
-  console.log(1)
-}
+const app = createApp(App)
+app.use(store).use(router).mount('#app')
