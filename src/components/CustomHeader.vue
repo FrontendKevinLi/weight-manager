dc37bd56b3adac394494e295d0af203b6b19520f
kevin12509673 <kayiu99623@gmail.com>
2022-10-27 18:36:43 +0800

    Alter: remove deprecated animation

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -8,7 +8,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import gsap from 'gsap'
 
 import ProfilePng from '@/assets/header-icons/profile.png'
 import BellSvg from '@/assets/header-icons/bell-regular.svg'
@@ -22,27 +21,6 @@ export default defineComponent({
       ProfilePng,
       BellSvg,
     }
-  },
-  mounted() {
-    this.initAnimations()
-  },
-  methods: {
-    initAnimations() {
-      const timeline = gsap.timeline()
-
-      timeline.from('.user-info', {
-        x: -150,
-        opacity: 0,
-        ease: 'back',
-        duration: 0.5,
-      }, 0)
-      timeline.from('.page-name', {
-        x: 150,
-        opacity: 0,
-        ease: 'back',
-        duration: 0.5,
-      }, 0)
-    },
   },
 })
 </script>
