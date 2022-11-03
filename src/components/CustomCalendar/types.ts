f64b45af8c8ab96e8a96ba4c04e615d50fdf7978
kevin12509673 <kayiu99623@gmail.com>
2022-11-03 17:56:35 +0800

    Alter: add types

--- src/components/CustomCalendar/types.ts
+++ src/components/CustomCalendar/types.ts
@@ -1,5 +1,13 @@
+import { DateTime } from 'luxon'
+import { CustomDialogProps } from '@/components/CustomDialog/types'
+
 export type CalendarItem = {
   weekdayShort: string,
   day: number,
+  dateTime: DateTime,
   isTargetMonth: boolean
 }
+
+export type CalendarItemDialogProps = {
+  calendarItem: CalendarItem
+} & CustomDialogProps
