32ca797c9dc3246dabf6b3474176dff2a8f6a7a2
kevin12509673 <kayiu99623@gmail.com>
2022-11-15 22:35:53 +0800

    Optimize: add terser plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
@@ -10,42 +11,55 @@ module.exports = defineConfig({
   configureWebpack: {
     plugins: [
     ].concat(isProduction ? [] : [new BundleAnalyzerPlugin()]),
-  },
-  chainWebpack: (config) => {
-    config.optimization.splitChunks({
-      chunks: 'all',
-      cacheGroups: {
-        libs: {
-          name: 'chunk-libs',
-          priority: 10,
-          test: /[\\/]node_modules[\\/]/,
-        },
-        luxon: {
-          name: 'chunk-luxon',
-          priority: 15,
-          test: /[\\/]node_modules[\\/]_?luxon(.*)/,
-        },
-        gsap: {
-          name: 'chunk-gsap',
-          priority: 20,
-          test: /[\\/]node_modules[\\/]_?gsap(.*)/,
-        },
-        firebase: {
-          name: 'chunk-firebase',
-          priority: 25,
-          test: /[\\/]node_modules[\\/]_?@firebase(.*)/,
-        },
-        echarts: {
-          name: 'chunk-echarts',
-          priority: 30,
-          test: /[\\/]node_modules[\\/]_?echarts(.*)/,
-        },
-        vue: {
-          name: 'chunk-vue',
-          priority: 35,
-          test: /[\\/]node_modules[\\/]_?@vue(.*)/,
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          minify: TerserPlugin.uglifyJsMinify,
+          terserOptions: {
+            compress: true,
+          },
+          test: /\.js(\?.*)?$/i,
+        }),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            priority: 10,
+            test: /[\\/]node_modules[\\/]/,
+          },
+          luxon: {
+            name: 'chunk-luxon',
+            priority: 15,
+            test: /[\\/]node_modules[\\/]_?luxon(.*)/,
+          },
+          gsap: {
+            name: 'chunk-gsap',
+            priority: 20,
+            test: /[\\/]node_modules[\\/]_?gsap(.*)/,
+          },
+          firebase: {
+            name: 'chunk-firebase',
+            priority: 25,
+            test: /[\\/]node_modules[\\/]_?@firebase(.*)/,
+          },
+          echarts: {
+            name: 'chunk-echarts',
+            priority: 30,
+            test: /[\\/]node_modules[\\/]_?echarts(.*)/,
+          },
+          vue: {
+            name: 'chunk-vue',
+            priority: 35,
+            test: /[\\/]node_modules[\\/]_?@vue(.*)/,
+          },
         },
       },
-    })
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
   },
 })
