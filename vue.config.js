7607c267e7a96fe3ccc49b0faa4d583d2feffdb9
kevin12509673 <kayiu99623@gmail.com>
2022-11-15 12:50:41 +0800

    Optimization: split chunks

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/weight-manager/'
     : '/',
-}
+  chainWebpack: (config) => {
+    config.optimization.splitChunks({
+      chunks: 'all',
+    })
+  },
+})
