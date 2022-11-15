10e7673376bec58c9a752121def7ab2984db7914
kevin12509673 <kayiu99623@gmail.com>
2022-11-15 15:57:53 +0800

    Optimization: split chunks

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,40 @@
 const { defineConfig } = require('@vue/cli-service')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/weight-manager/'
     : '/',
+  configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin(),
+    ],
+  },
   chainWebpack: (config) => {
     config.optimization.splitChunks({
       chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          priority: 10,
+          test: /[\\/]node_modules[\\/]/,
+        },
+        gsap: {
+          name: 'chunk-gsap',
+          priority: 20,
+          test: /[\\/]node_modules[\\/]_?gsap(.*)/,
+        },
+        luxon: {
+          name: 'chunk-luxon',
+          priority: 25,
+          test: /[\\/]node_modules[\\/]_?luxon(.*)/,
+        },
+        echarts: {
+          name: 'chunk-echarts',
+          priority: 30,
+          test: /[\\/]node_modules[\\/]_?echarts(.*)/,
+        },
+      },
     })
   },
 })
