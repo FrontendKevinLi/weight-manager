822509d87de2496a903105e86154539275dae8ec
kevin12509673 <kayiu99623@gmail.com>
2022-11-21 16:02:02 +0800

    Alter: add close button

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <IconButton
+    class="close-button"
+    :value="{svgSrc: XMarkSvg}"
+  />
+</template>
+
+<script lang="ts" setup>
+import IconButton from '@/components/IconButton.vue'
+import XMarkSvg from '@/assets/dialog/xmark-solid.svg'
+
+</script>
+
+<style lang="scss" scoped>
+.close-button {
+  width: 40px;
+}
+</style>
